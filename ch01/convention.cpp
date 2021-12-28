e2c855fc9e1af0a49912812992d935297455131d
Park-HY-00 <phy000115@naver.com>
2021-12-29 01:04:32 +0900

    Chapter 01_3

--- ch01/convention.cpp
+++ ch01/convention.cpp
@@ -1,12 +1,15 @@
-class box{
-public:
-	int x,y,width,height;
-};
-int range_sum ( int a,int b )
+class box
 {
-	int sum=0;
-	for( int i=a ; i<= b ; i++ ){
-		sum+=i;
+public:
+	int x, y, width, height;
+};
+
+int range_sum(int a, int b)
+{
+	int sum = 0;
+	for (int i = a; i <= b; i++)
+	{
+		sum += i;
 	}
 	return sum;
 }
